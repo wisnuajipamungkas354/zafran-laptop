4748c0fe9b540764498286488c0e1cbd9eb012b5
Wisnu Aji Pamungkas <wisnuajipamungkas354@gmail.com>
2025-06-18 10:19:44 +0700

    install tailwindcss

---
+++ database/migrations/2025_06_16_142307_create_transactions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('orders')->cascadeOnDelete();
+            $table->string('payment_type');// Cash, Transfer, Gopay, QRIS, Dana dll
+            $table->string('transaction_id')->nullable(); // ID dari Midtrans
+            $table->string('status'); // pending, settlement, expired, etc.
+            $table->decimal('amount', 10, 2);
+            $table->json('response')->nullable(); // simpan response json Midtrans
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,5 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
 });
